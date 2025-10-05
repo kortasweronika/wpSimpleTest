58a9fbf81285203824d031cfad06829c91b10961
Weronika Kortas <weronika.kortas@b2bnetwork.pl>
2025-10-05 10:26:19 +0200

    prosty bash

--- src/main/report.sh
+++ src/main/report.sh
@@ -8,17 +8,11 @@ if [[ -z "$UUID" ]]; then
   exit 1
 fi
 
-# (opcjonalnie) prosta walidacja formatu UUID
-if ! [[ "$UUID" =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$ ]]; then
-  echo "Błędny format UUID: $UUID" >&2
-  exit 2
-fi
-
 echo "Hello world"
 
-FILENAME="${UUID}_report.csv"
+FILENAME="id_${UUID}_report.csv"
 
 # Zapisz dokładnie jedną linię w żądanym formacie i stałych datach
-printf "%s|Parametry przekazane poprawnie|2025-10-01 17:06:42|2025-10-01 17:06:42|PASS\n" "$UUID" > "$FILENAME"
+printf "id_%s|Parametry przekazane poprawnie|2025-10-01 17:06:42|2025-10-01 17:06:42|PASS\n" "$UUID" > "$FILENAME"
 
 echo "Utworzono plik: $FILENAME"
